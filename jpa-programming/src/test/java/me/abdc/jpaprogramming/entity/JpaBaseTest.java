27bc8baaee201acea440f409efd31be0450cbe1d
abdc06 <abdc2806@gmail.com>
2022-10-22 19:59:38 +0900

    test package 변경

--- jpa-programming/src/test/java/me/abdc/jpaprogramming/entity/JpaBaseTest.java
+++ jpa-programming/src/test/java/me/abdc/jpaprogramming/entity/JpaBaseTest.java
@@ -1,13 +1,10 @@
-package me.abdc.jpaprogramming.jpa;
+package me.abdc.jpaprogramming.entity;
 
-import me.abdc.jpaprogramming.entity.Account;
 import org.hibernate.Session;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
