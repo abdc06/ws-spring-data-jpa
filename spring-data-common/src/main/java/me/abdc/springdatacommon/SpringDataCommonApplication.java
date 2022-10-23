54eeeac2152a1662b7886dc95b8e09a2930a39a7
abdc06 <abdc2806@gmail.com>
2022-10-23 11:33:45 +0900

    spring-data-common 프로젝트 생성

---
+++ spring-data-common/src/main/java/me/abdc/springdatacommon/SpringDataCommonApplication.java
@@ -0,0 +1,13 @@
+package me.abdc.springdatacommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataCommonApplication.class, args);
+    }
+
+}
---
+++ spring-data-common/src/test/java/me/abdc/springdatacommon/SpringDataCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abdc.springdatacommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
