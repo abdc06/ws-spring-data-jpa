583c98d143f4610c4b9742cf1f381730497ec95d
abdc06 <abdc2806@gmail.com>
2022-10-23 21:22:49 +0900

    null, nonnull 처리

--- spring-data-common/src/main/java/me/abdc/springdatacommon/repository/MyRepository.java
+++ spring-data-common/src/main/java/me/abdc/springdatacommon/repository/MyRepository.java
@@ -3,6 +3,8 @@ package me.abdc.springdatacommon.repository;
 import me.abdc.springdatacommon.entity.Comment;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 import java.io.Serializable;
 import java.util.List;
@@ -13,9 +15,10 @@ public interface MyRepository<T, Id extends Serializable> extends Repository<T, 
 
     List<T> findAll();
 
-    Optional<T> findById(Id id);
+    @Nullable
+    <E extends T> Optional<E> findById(Id id);
 
-    <E extends T> E save(E entity);
+    <E extends T> E save(@NonNull E entity);
 
     void deleteById(Id id);
 }
--- spring-data-common/src/test/java/me/abdc/springdatacommon/repository/CommentRepositoryTest.java
+++ spring-data-common/src/test/java/me/abdc/springdatacommon/repository/CommentRepositoryTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,10 +20,26 @@ class CommentRepositoryTest {
     CommentRepository commentRepository;
 
     @Test
-    void crud() {
+    void select() {
         List<Comment> comments = commentRepository.findAll();
         long count = commentRepository.count();
 
         assertEquals(count, comments.size());
     }
+
+    @Test
+    void selectOne() {
+        Optional<Comment> byId = commentRepository.findById(100L);
+
+        assertTrue(byId.isPresent());
+        assertFalse(byId.isEmpty());
+    }
+
+    @Test
+    void nullCheck() {
+        assertThrows(InvalidDataAccessApiUsageException.class, () -> {
+            commentRepository.save(null);
+        });
+    }
+        
 }
