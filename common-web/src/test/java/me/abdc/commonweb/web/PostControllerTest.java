e10772307517b3931779f5127f82103b138b4c36
abdc06 <abdc2806@gmail.com>
2022-10-25 21:46:23 +0900

    Pageble 테스트

--- common-web/src/test/java/me/abdc/commonweb/web/PostControllerTest.java
+++ common-web/src/test/java/me/abdc/commonweb/web/PostControllerTest.java
@@ -10,11 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -37,6 +37,23 @@ class PostControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("JPA"));
+    }
+
+    @Test
+    void getPosts() throws Exception {
+        Post post = new Post();
+        post.setTitle("JPA");
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts")
+                        .param("page", "0")
+                        .param("size", "10")
+                        .param("sort", "createdOn,desc")
+                        .param("sort", "title"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].title", is("JPA")));
+
     }
 
 }
