ffae082411b7f3d102d3c2f85291138be4aaf874
abdc06 <abdc2806@gmail.com>
2022-10-25 12:49:33 +0900

    DomainClassConverter 테스트 코드

---
+++ common-web/src/test/java/me/abdc/commonweb/web/PostControllerTest.java
@@ -0,0 +1,42 @@
+package me.abdc.commonweb.web;
+
+import me.abdc.commonweb.entity.Post;
+import me.abdc.commonweb.repository.PostRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+class PostControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    void getPost() throws Exception {
+        Post post = new Post();
+        post.setTitle("JPA");
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("JPA"));
+    }
+
+}
