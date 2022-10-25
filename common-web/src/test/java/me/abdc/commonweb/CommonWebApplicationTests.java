2d6087a988b6fff213790cd8823d94d03f579ae5
abdc06 <abdc2806@gmail.com>
2022-10-25 12:30:16 +0900

    common-web 프로젝트 생성

---
+++ common-web/src/test/java/me/abdc/commonweb/CommonWebApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abdc.commonweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
