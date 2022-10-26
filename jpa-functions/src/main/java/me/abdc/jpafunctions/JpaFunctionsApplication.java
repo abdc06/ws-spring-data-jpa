8d6253b53f0100330966403ba1c852c05fa2dff6
abdc06 <abdc2806@gmail.com>
2022-10-26 12:52:25 +0900

    jpa-functions 프로젝트 생성

---
+++ jpa-functions/src/main/java/me/abdc/jpafunctions/JpaFunctionsApplication.java
@@ -0,0 +1,13 @@
+package me.abdc.jpafunctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaFunctionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaFunctionsApplication.class, args);
+    }
+
+}
---
+++ jpa-functions/src/test/java/me/abdc/jpafunctions/JpaFunctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abdc.jpafunctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaFunctionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
