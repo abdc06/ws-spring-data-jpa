0139fb2de7bdcc47bf2771934a2d3fc7d8d5ffdf
abdc06 <abdc2806@gmail.com>
2022-10-27 08:43:57 +0900

    JPA 쿼리 메소드

--- jpa-functions/src/main/java/me/abdc/jpafunctions/repository/PostRepository.java
+++ jpa-functions/src/main/java/me/abdc/jpafunctions/repository/PostRepository.java
@@ -3,5 +3,29 @@ package me.abdc.jpafunctions.repository;
 import me.abdc.jpafunctions.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    /**
+     * TODO And, Or
+     * TODO Is, Equals
+     * TODO LessThan, LessThanEqual, GreaterThan, GreaterThanEqual
+     * TODO After, Before
+     * TODO IsNull, IsNotNull, NotNull
+     * TODO Like, NotLike
+     * TODO StartingWith, EndingWith, Containing
+     * TODO OrderBy
+     * TODO Not, In, NotIn
+     * TODO True, False
+     * TODO IgnoreCase
+     */
+    List<Post> findByTitleStartsWith(String title);
+
+    List<Post> findByHitsGreaterThanEqual(int hits);
+
+    List<Post> findByTitleIsNotNull();
+
+    Optional<Post> findByTitleIgnoreCase(String title);
+
 }
